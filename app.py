b00073ee9c74c2ff184c9c891a9825ac39d669f0
Corbett Austin <austinc@sualumni.net>
2020-06-16 21:12:59 -0500

    database setup

--- app.py
+++ app.py
@@ -15,6 +15,17 @@ from flask import Flask, jsonify
 #################################################
 engine = create_engine("sqlite:///hawaii.sqlite")
 
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
 
 #################################################
 # Flask Setup
